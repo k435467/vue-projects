6c58265ff025d58bf588f55a8a39c64823c1058a
Ethan Guo <k435467@gmail.com>
2023-04-23 01:01:44 +0800

    qs display, results

--- random-user/src/App.vue
+++ random-user/src/App.vue
@@ -1,17 +1,38 @@
 <script setup lang="ts">
+import { useDisplay, useResults } from "@/utils";
+
 const path = {
   home: "/",
   favorite: "/favorite",
 };
+
+const { results, onResultSelectChange } = useResults();
+const { display, changeDisplayMode } = useDisplay();
 </script>
 
 <template lang="pug">
 div(class="container mx-auto p-8")
-  div(class="flex justify-between mb-8")
+  div(class="flex justify-between items-center mb-8")
     div(class="flex space-x-4")
-      router-link(:to="path?.home" class="opacity-50 text-lg underline text-violet-700" :class="{'opacity-100': $route.path === path?.home}") All
-      router-link(:to="path?.favorite" class="opacity-50 text-lg underline text-violet-700" :class="{'opacity-100': $route.path === path?.favorite}") Favorite
-    div something
+      router-link(
+        :to="path?.home"
+        class="opacity-50 text-lg underline text-violet-700"
+        :class="{'!opacity-100': $route.path === path?.home}"
+      ) All
+      router-link(
+        :to="path?.favorite"
+        class="opacity-50 text-lg underline text-violet-700"
+        :class="{'!opacity-100': $route.path === path?.favorite}"
+      ) Favorite
+    div(class="flex space-x-2 items-center")
+      select(class="border rounded" :value="results.toString()" @change="onResultSelectChange($event)")
+        option(value="10") 10
+        option(value="30") 30
+        option(value="50") 50
+      button(@click="changeDisplayMode('list')")
+        v-icon(name="ri-menu-fill" class="text-violet-700")
+      button(@click="changeDisplayMode('grid')")
+        v-icon(name="ri-layout-grid-line" class="text-violet-700")
   div
     router-view/
 </template>
--- random-user/src/main.ts
+++ random-user/src/main.ts
@@ -3,10 +3,13 @@ import { createPinia } from "pinia";
 import { router } from "@/router";
 import "./style.css";
 import App from "./App.vue";
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { RiMenuFill, RiLayoutGridLine } from "oh-vue-icons/icons";
+
+addIcons(RiMenuFill, RiLayoutGridLine);
 
 const pinia = createPinia();
 const app = createApp(App);
 
-app.use(router);
-app.use(pinia);
-app.mount("#app");
+app.component("v-icon", OhVueIcon);
+app.use(router).use(pinia).mount("#app");
